247cd17def559c21a87562353301fadc4f3551a0
syahruladi7 <syahrulsaputra6366@gmail.com>
2026-04-21 17:11:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
-
-# Create flask app
-flask_app = Flask(__name__)
-model = pickle.load(open("linear_regression_model.pkl", "rb"))
-
-@flask_app.route("/")
-def Home():
-    return render_template("index.html")
-
-@flask_app.route("/predict", methods = ["POST"])
-def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+import os
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    # Render akan memberikan port melalui os.environ.get('PORT')
+    # Gunakan 5000 sebagai fallback untuk lokal
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
