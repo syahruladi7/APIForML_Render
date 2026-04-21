7beb870f58fc886eee70b079dfbc3d6ff0c18f1b
syahruladi7 <syahrulsaputra6366@gmail.com>
2026-04-21 17:21:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from flask import Flask
+import joblib
+import pandas as pd
 import os
 
-if __name__ == "__main__":
-    # Render akan memberikan port melalui os.environ.get('PORT')
-    # Gunakan 5000 sebagai fallback untuk lokal
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "API ML Berjalan!"
+
+if _name_ == "_main_":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
